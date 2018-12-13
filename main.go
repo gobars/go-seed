c9997a41e3a22a1d6d21ff97e4a7c32c318a56ec
shuvigoss <shuvigoss@gmail.com>
2018-12-13 11:29:34 +0800

    fix folder exsit error

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"text/template"
 	"bytes"
 	"io/ioutil"
+	"path/filepath"
+	"io"
 )
 
 type Context struct {
@@ -49,12 +51,19 @@ func main() {
 		createFile(target, f)
 	}
 
-	if err := os.Rename(path.Join(dir, tempDir, root(*p)), path.Join(dir, root(*p))); err != nil {
+	if err := copyDir(path.Join(dir, tempDir, root(*p)), path.Join(dir, root(*p))); err != nil {
 		cleanProject(path.Join(dir, tempDir))
 		fmt.Println("fail to create project " + *p + " with err " + err.Error())
 		os.Exit(0)
 		return
 	}
+
+	//if err := os.Rename(path.Join(dir, tempDir, root(*p)), path.Join(dir, root(*p))); err != nil {
+	//	cleanProject(path.Join(dir, tempDir))
+	//	fmt.Println("fail to create project " + *p + " with err " + err.Error())
+	//	os.Exit(0)
+	//	return
+	//}
 	cleanProject(path.Join(dir, tempDir))
 	fmt.Println("success create project " + *p)
 
@@ -94,4 +103,97 @@ func createFile(target string, inner string) {
 		}
 
 	}
+}
+
+func copyDir(src string, dst string) (err error) {
+	src = filepath.Clean(src)
+	dst = filepath.Clean(dst)
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	if !si.IsDir() {
+		return fmt.Errorf("source is not a directory")
+	}
+
+	_, err = os.Stat(dst)
+	if err != nil && !os.IsNotExist(err) {
+		return
+	}
+	if err != nil {
+		err = os.MkdirAll(dst, si.Mode())
+	}
+
+	if err != nil {
+		return
+	}
+
+	entries, err := ioutil.ReadDir(src)
+	if err != nil {
+		return
+	}
+
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		dstPath := filepath.Join(dst, entry.Name())
+
+		if entry.IsDir() {
+			err = copyDir(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		} else {
+			// Skip symlinks.
+			if entry.Mode()&os.ModeSymlink != 0 {
+				continue
+			}
+
+			err = CopyFile(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
+}
+
+func CopyFile(src, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer func() {
+		if e := out.Close(); e != nil {
+			err = e
+		}
+	}()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return
+	}
+
+	err = out.Sync()
+	if err != nil {
+		return
+	}
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+	err = os.Chmod(dst, si.Mode())
+	if err != nil {
+		return
+	}
+
+	return
 }
